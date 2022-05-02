95b9a3d4c72bc5d689961d8958b751268cef5a1f
Anastasiya Khvashcheuskaya <nasta30051997@gmail.com>
2022-05-02 23:13:47 +0300

    feat: add third task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let abbr = [];
+
+  if((typeof(members) != 'object') || !!!members || !isNaN(members) || !((Object.keys(members))[0]<=1))
+    return false;
+
+  members.forEach(el => {
+    if (!!!el || typeof(el) != 'string' || (el === null)) {
+      return false;
+    }
+    else {
+      el = el.replaceAll(" ", "");
+      abbr.push(el[0].toUpperCase());
+    }
+  });
+
+  return abbr.sort().join('');
 }
 
 module.exports = {
